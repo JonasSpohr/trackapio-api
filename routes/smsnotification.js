32b776846f2e5c8f6d97e531ce505fede5e30e84
Jonas Spohr <jonasspohr@gmai.com>
2018-07-16 13:09:25 -0700

    answer sms code

--- routes/smsnotification.js
+++ routes/smsnotification.js
@@ -4,11 +4,11 @@ const asyncHandler = require('express-async-handler');
 const moment = require('moment');
 
 router.post('/answer', asyncHandler(async (req, res) => {
-  return res.send({ success: true, result: JSON.stringify(req.body) });
+  return res.send({ success: true, result: req.body });
 }));
 
 router.post('/fail', asyncHandler(async (req, res) => {
-  return res.send({ success: true, result: JSON.stringify(req.body) });
+  return res.send({ success: true, result: req.body });
 }));
 
 module.exports = router;
